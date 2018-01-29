d293baa87236816f5c198d53da162e8836e4e633
jiveshanand <35930015+jiveshanand@users.noreply.github.com>
2018-01-30 02:23:46 +0530

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,6 +1,7 @@
 function light(s){
 if(s=1){
-  document.getElementById("demo").innerHTML.src="pic_bulboff.gif";
+ document.getElementById("demo").innerHTML="hi";
+  // document.getElementById("demo").innerHTML.src="pic_bulboff.gif";
 }
   else{
     document.getElementById("demo").innerHTML.src="pic_bulbon.gif";
